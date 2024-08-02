44ad123d6067ad7fd3f04c15796d62e3aa5857a8
CHISANW <keuye06380618@gmail.com>
2024-08-02 20:22:35 +0900

    feat : JPA JpaAuditing 설정 추가

---
+++ src/main/java/team9499/commitbody/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package team9499.commitbody.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
