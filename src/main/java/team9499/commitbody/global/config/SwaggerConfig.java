e1ed75463098595a51014d508646852e97e8fd65
CHISANW <keuye06380618@gmail.com>
2024-08-02 21:42:20 +0900

    feat : swagger jwt 토큰 설정 추가

--- src/main/java/team9499/commitbody/global/config/SwaggerConfig.java
+++ src/main/java/team9499/commitbody/global/config/SwaggerConfig.java
@@ -2,9 +2,13 @@ package team9499.commitbody.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @OpenAPIDefinition(
         info = @Info(title = "커밋 바디 API",
@@ -16,7 +20,25 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openApi(){
-        return new OpenAPI();
+
+        String socialToken ="SocialToken";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(socialToken);
+
+        SecurityScheme socialTokenSc = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        Components components = new Components()
+                .addSecuritySchemes(socialToken, socialTokenSc);
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 
 }
