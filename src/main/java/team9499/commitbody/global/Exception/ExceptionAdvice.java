f3e577443f55297637032eee13ae82946c91a8fc
CHISANW <keuye06380618@gmail.com>
2024-08-02 18:11:42 +0900

    feat : Exception Handler 및 Custom Exception 구현
    - 전역 예외를 처리하기 위한 ExceptionAdvice, ExceptionHandler 기능 구현
    - Custom Exception 작성

---
+++ src/main/java/team9499/commitbody/global/Exception/ExceptionAdvice.java
@@ -0,0 +1,24 @@
+package team9499.commitbody.global.Exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import team9499.commitbody.global.payload.ErrorResponse;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> serverException(ServerException e){
+        ErrorResponse er = new ErrorResponse<>(false, e.getMessage());
+        e.printStackTrace();
+        return ResponseEntity.status(500).body(er);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> noSuchException(NoSuchException e){
+        ErrorResponse er = new ErrorResponse(false, e.getMessage());
+        e.printStackTrace();
+        return ResponseEntity.status(400).body(er);
+    }
+}
