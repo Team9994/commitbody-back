3520b9266a997425a97322b52bf864ecc7e1a786
CHISANW <keuye06380618@gmail.com>
2024-08-03 04:34:24 +0900

    fix : 회원가입/로그인 컨트롤러 수정
    - 쿠키로 토큰을 전달하지 않고 ResponseBody의 전달하도록 수정
    - Header에서 jwt토큰을 추출하는 기능을 메소드로 추출

--- src/main/java/team9499/commitbody/global/authorization/controller/AuthorizationController.java
+++ src/main/java/team9499/commitbody/global/authorization/controller/AuthorizationController.java
@@ -2,14 +2,11 @@ package team9499.commitbody.global.authorization.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team9499.commitbody.domain.Member.domain.LoginType;
 import team9499.commitbody.global.authorization.service.AuthorizationService;
 import team9499.commitbody.global.payload.SuccessResponse;
-import team9499.commitbody.global.utils.CookieUtils;
 
 import java.util.Map;
 
@@ -20,23 +17,19 @@ public class AuthorizationController {
 
     private final AuthorizationService authorizationService;
 
-    private final String ACCESS_TOKEN = "accessToken";
-    private final String REFRESH_TOKEN = "refreshToken";
-
     @PostMapping("/auth")
     public ResponseEntity<?> socialLogin(@RequestParam("type")LoginType loginType,
                                          HttpServletRequest request){
+        String jwtToken = getJwtToken(request);
+
+        Map<String, String> jwtTokenMap = authorizationService.authenticateOrRegisterUser(loginType, jwtToken);
+
+        return ResponseEntity.ok().body(new SuccessResponse<>(true,"성공",jwtTokenMap));
+    }
+
+
+    private static String getJwtToken(HttpServletRequest request) {
         String authorization = request.getHeader("Authorization");
-        String jwtToken = authorization.replace("Bearer ", "");
-
-        Map<String, String> map = authorizationService.authenticateOrRegisterUser(loginType, jwtToken);
-
-        ResponseCookie accessCookie = CookieUtils.createCookie(ACCESS_TOKEN, map.get(ACCESS_TOKEN));
-        ResponseCookie refreshCookie = CookieUtils.createCookie(REFRESH_TOKEN, map.get(REFRESH_TOKEN));
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE,accessCookie.toString())
-                .header(HttpHeaders.SET_COOKIE,refreshCookie.toString())
-                .body(new SuccessResponse<>(true,"성공"));
+        return authorization.replace("Bearer ", "");
     }
 }
