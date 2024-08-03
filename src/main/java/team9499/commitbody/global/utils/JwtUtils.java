21bf476056bc1f2eafa8133e34ef700a0155687d
CHISANW <keuye06380618@gmail.com>
2024-08-03 16:32:17 +0900

    feat : JwtUtils 토큰 검증 구현
    - 토큰에 대한 정보와 만료되었는지에 대한 기능을 추가

--- src/main/java/team9499/commitbody/global/utils/JwtUtils.java
+++ src/main/java/team9499/commitbody/global/utils/JwtUtils.java
@@ -3,15 +3,20 @@ package team9499.commitbody.global.utils;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import team9499.commitbody.domain.Member.dto.MemberDto;
+import team9499.commitbody.global.Exception.ExceptionType;
+import team9499.commitbody.global.Exception.JwtTokenException;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Map;
 
+@Slf4j
 @Component
 public class JwtUtils {
 
@@ -57,7 +62,38 @@ public class JwtUtils {
     private String creatToken(MemberDto memberDto, Date expirationDate) {
         return  JWT.create()
                 .withExpiresAt(expirationDate)
-                .withClaim(MEMBER_ID, memberDto.getMemberId())
+                .withClaim(MEMBER_ID, String.valueOf(memberDto.getMemberId()))
                 .sign(Algorithm.HMAC512(SECRET));
+    }
+
+    public String accessTokenValid(String jwtToken){
+        try {
+            if (!tokenExpiration(jwtToken)) {
+                return verifyToken(jwtToken);
+            }
+            return null;
+        } catch (SignatureVerificationException e) {
+            log.error("존재하지 않은 토큰 사용");
+            throw new JwtTokenException(ExceptionType.TOKEN_NOT_FOUND);
+        }
+    }
+
+    /*
+    토큰의 정보를 추출하는 메서드
+     */
+    public String verifyToken(String tokenValue) {
+        return JWT.require(Algorithm.HMAC512(SECRET)).build()
+                .verify(tokenValue)
+                .getClaim(MEMBER_ID)
+                .asString();
+
+    }
+
+    /*
+    토큰의 대한 시간 검증 (true : 토큰 만료 , false : 토큰 유효)
+     */
+    public Boolean tokenExpiration(String token) {
+        Date expired = JWT.decode(token).getExpiresAt();
+        return expired != null && expired.before(new Date());
     }
 }
