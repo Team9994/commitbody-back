a7397db0eda13a38bfc82007a54314d0353539b3
CHISANW <keuye06380618@gmail.com>
2024-08-04 22:16:52 +0900

    feat : 사용할 수 없는 데이터 예외 작성

--- src/main/java/team9499/commitbody/global/Exception/ExceptionType.java
+++ src/main/java/team9499/commitbody/global/Exception/ExceptionType.java
@@ -9,7 +9,8 @@ public enum ExceptionType {
     INVALID_TOKEN_MESSAGE("사용할 수 없는 토큰입니다."),
     TOKEN_EXPIRED("만료된 토큰 입니다."),
     LOGIN_REQUIRED("접근하려는 페이지에 접근하려면 로그인해야 합니다."),
-    ACCESS_DENIED("이 리소스에 접근할 권한이 없습니다.");
+    ACCESS_DENIED("이 리소스에 접근할 권한이 없습니다."),
+    DUPLICATE_NICKNAME("중복된 닉네임 입니다.");
 
 
     private final String message;
