0f17dfa2efd3aa1aa596796bfc89895f100e98f6
CHISANW <keuye06380618@gmail.com>
2024-08-06 21:20:30 +0900

    remove : 기존 테스트 에노테이션 삭제

--- src/test/java/team9499/commitbody/CommitbodyApplicationTests.java
+++ src/test/java/team9499/commitbody/CommitbodyApplicationTests.java
@@ -3,7 +3,7 @@ package team9499.commitbody;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CommitbodyApplicationTests {
 
 	@Test
