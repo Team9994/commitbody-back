85958cf0bb8aaf8f778ac2ac09942a7681bb0a57
CHISANW <keuye06380618@gmail.com>
2024-08-08 03:46:40 +0900

    rename : ExerciseMethod 폴더 이동

--- src/main/java/team9499/commitbody/domain/exercise/repository/ExerciseMethodRepository.java
+++ src/main/java/team9499/commitbody/domain/exercise/repository/ExerciseMethodRepository.java
@@ -2,7 +2,7 @@ package team9499.commitbody.domain.exercise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import team9499.commitbody.domain.exercise.domain.enums.ExerciseMethod;
+import team9499.commitbody.domain.exercise.domain.ExerciseMethod;
 
 @Repository
 public interface ExerciseMethodRepository extends JpaRepository<ExerciseMethod, Long> {
