beb74aafc31e15d063058308d5148ed205f74590
CHISANW <keuye06380618@gmail.com>
2024-08-08 05:21:23 +0900

    feat : 실행시간, @Valid 반환 AOP 구현
    - Controller 단에서 실행되는 시간을 측정하도록 AOP를 구현
    - BindingResult 부분이 동일한 코드가 반복적으로 사용되어 관심사를 분리하기 위해서 AOP를 적용

--- src/main/java/team9499/commitbody/global/aop/AspectAdvice.java
+++ src/main/java/team9499/commitbody/global/aop/AspectAdvice.java
@@ -1,2 +1,60 @@
-package team9499.commitbody.global.aop;public class AspectAdvice {
+package team9499.commitbody.global.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import team9499.commitbody.global.payload.ErrorResponse;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Slf4j
+@Aspect
+@Component("aspectAdvice")
+public class AspectAdvice {
+
+    @Around("team9499.commitbody.global.aop.Pointcuts.executionTime()")
+    public Object executionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        String methodName = joinPoint.getSignature().toLongString();
+
+        try {
+            log.info("start method: {}", methodName);
+            Object result = joinPoint.proceed();
+
+            long executionTime = System.currentTimeMillis() - start;
+            log.info("end method:{} time in {} ms", methodName, executionTime);
+
+            return result;
+        } catch (Throwable e) {
+            log.error("Exception in method {}: {}", methodName, e.getMessage());
+            throw e;
+        }
+    }
+
+    @Around("team9499.commitbody.global.aop.Pointcuts.validResponse()")
+    public Object validResponse(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+
+        for (Object arg : args){
+            if (arg instanceof BindingResult){
+                BindingResult result = (BindingResult) arg;
+                if (result.hasErrors()){
+                    Map<String,String> errorMap = new LinkedHashMap<>();
+                    for(FieldError error : result.getFieldErrors()){
+                        errorMap.put(error.getField(),error.getDefaultMessage());
+                    }
+                    return ResponseEntity.status(BAD_REQUEST).body(new ErrorResponse<>(false, "실패", errorMap));
+                }
+            }
+        }
+        return joinPoint.proceed();
+    }
 }
--- src/main/java/team9499/commitbody/global/aop/Pointcuts.java
+++ src/main/java/team9499/commitbody/global/aop/Pointcuts.java
@@ -1,2 +1,15 @@
-package team9499.commitbody.global.aop;public class Pointcuts {
+package team9499.commitbody.global.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class Pointcuts {
+
+    @Pointcut("execution(* *..*Controller.*(..))")
+    void executionTime(){}
+
+    @Pointcut("execution(* *..*Controller.*(..))")
+    void validResponse(){}
+
 }
