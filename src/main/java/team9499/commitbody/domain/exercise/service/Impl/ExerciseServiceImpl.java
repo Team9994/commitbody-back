a860a45dd75e884943dbc0a994c04f075241aa2e
CHISANW <keuye06380618@gmail.com>
2024-08-12 06:43:27 +0900

    refactor : 운동 검색 리팩토링
    - 운동 검색시 이미지를 등록하지 않았을때 삼항 연산자를 통해 NULL일때 "등록된 이미지 파일이 없습니다." 메시지가 출력 되도록 리팩토링

--- src/main/java/team9499/commitbody/domain/exercise/service/Impl/ExerciseServiceImpl.java
+++ src/main/java/team9499/commitbody/domain/exercise/service/Impl/ExerciseServiceImpl.java
@@ -165,7 +165,7 @@ public class ExerciseServiceImpl implements ExerciseService {
                     Map<String,Object> source = (Map<String,Object>)hit.source();
                     linkedHashMap.put(EXERCISE_ID,source.get(EXERCISE_ID));
                     linkedHashMap.put(NAME_FIELD,source.get(NAME_FIELD));
-                    linkedHashMap.put(GIF_URL,source.get(GIF_URL));
+                    linkedHashMap.put(GIF_URL,source.get(GIF_URL) !=null? source.get(GIF_URL):"등록된 이미지 파일이 없습니다.");
                     linkedHashMap.put(TARGET_FIELD,source.get(TARGET_FIELD));
                     linkedHashMap.put(EXERCISE_TYPE,"무게와 횟수");
                     linkedHashMap.put(EQUIPMENT_FIELD,source.get(EQUIPMENT_FIELD));
