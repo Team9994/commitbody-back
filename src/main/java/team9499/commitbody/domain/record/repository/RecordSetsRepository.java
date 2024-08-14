e00fd43245fad7986cfb7a64023014815a10edb7
CHISANW <keuye06380618@gmail.com>
2024-08-14 21:22:16 +0900

    feat : 운동 기록 repository 작성

---
+++ src/main/java/team9499/commitbody/domain/record/repository/RecordSetsRepository.java
@@ -0,0 +1,9 @@
+package team9499.commitbody.domain.record.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team9499.commitbody.domain.record.domain.RecordSets;
+
+@Repository
+public interface RecordSetsRepository extends JpaRepository<RecordSets, Long> {
+}
