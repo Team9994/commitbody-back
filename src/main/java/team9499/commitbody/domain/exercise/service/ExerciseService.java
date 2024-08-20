27f933bc4ca88dcb3cfe383cfbf7485399353631
CHISANW <keuye06380618@gmail.com>
2024-08-20 16:11:15 +0900

    feat : 운동 상세조회 통계 service 작성
    - 조회에서 사용되는 메서드 이기 때문에 @Transactional(readOnly = true) 사용

--- src/main/java/team9499/commitbody/domain/exercise/service/ExerciseService.java
+++ src/main/java/team9499/commitbody/domain/exercise/service/ExerciseService.java
@@ -4,6 +4,7 @@ import org.springframework.web.multipart.MultipartFile;
 import team9499.commitbody.domain.exercise.domain.enums.ExerciseEquipment;
 import team9499.commitbody.domain.exercise.domain.enums.ExerciseTarget;
 import team9499.commitbody.domain.exercise.dto.SearchExerciseResponse;
+import team9499.commitbody.domain.exercise.dto.response.ExerciseResponse;
 
 public interface ExerciseService {
 
@@ -14,4 +15,6 @@ public interface ExerciseService {
     Long updateCustomExercise(String exerciseName, ExerciseTarget exerciseTarget, ExerciseEquipment exerciseEquipment, Long memberId, Long customExerciseId,MultipartFile file);
 
     void deleteCustomExercise(Long customExerciseId,Long memberId);
+
+    ExerciseResponse detailsExercise(Long exerciseId, Long memberId, String source);
 }
