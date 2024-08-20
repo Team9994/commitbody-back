9ab8f0c2a99d54cdbfa695d07c9e79cd71cc7148
CHISANW <keuye06380618@gmail.com>
2024-08-20 17:41:56 +0900

    feat : 상세운동 조회시 관심 운동 상태 확인 추가
    - 상세운동 진입시 현재 관심운동으로 등록되어 있는지 확인하는 기능을 추가
    - ExerciseResponse의 interestStatus필드 값 추가

--- src/main/java/team9499/commitbody/domain/exercise/service/ExerciseInterestService.java
+++ src/main/java/team9499/commitbody/domain/exercise/service/ExerciseInterestService.java
@@ -3,4 +3,6 @@ package team9499.commitbody.domain.exercise.service;
 public interface ExerciseInterestService {
 
     String updateInterestStatus(Long exerciseId, Long memberId, String source);
+
+    boolean checkInterestStatus(Long exerciseId, Long memberId);
 }
