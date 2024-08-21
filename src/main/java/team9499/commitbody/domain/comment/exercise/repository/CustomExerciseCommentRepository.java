e8c2d63395a2d9863f25bd41c7c14ad580f05bf9
CHISANW <keuye06380618@gmail.com>
2024-08-21 16:29:33 +0900

    feat : 운동 댓글 조회 무한 스크롤 로직 작성
    - 현재 조회할 운동의 작성된 댓글을 조회하는 무한 스크롤 방식의 기능을 구현
    - 작성된 시간을 며칠전, 1시간전으로 변환 되도록 컨버터 메서드 작성
    - lastId값을 통해 다음 페이지 조회
    
    Ref : #42

--- src/main/java/team9499/commitbody/domain/comment/exercise/repository/CustomExerciseCommentRepository.java
+++ src/main/java/team9499/commitbody/domain/comment/exercise/repository/CustomExerciseCommentRepository.java
@@ -1,4 +1,10 @@
 package team9499.commitbody.domain.comment.exercise.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import team9499.commitbody.domain.comment.exercise.dto.ExerciseCommentDto;
+
 public interface CustomExerciseCommentRepository {
+
+    Slice<ExerciseCommentDto> getExerciseComments(Long memberId, Long exerciseId, String source, Pageable pageable, Long lastId);
 }
