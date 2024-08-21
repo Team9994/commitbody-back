fb727e1da6ac10760822c9b2130f914e1496f6ca
CHISANW <keuye06380618@gmail.com>
2024-08-21 12:39:17 +0900

    docs : 운동 댓글 등록 Swagger 작성
    - @RequestBody에서 class를 사용하지않고 직접 Map을 사용하여 @Parameter을 사용하여 예시 타입 작성
    
    Ref : #42

--- src/main/java/team9499/commitbody/domain/comment/exercise/controller/ExerciseCommentController.java
+++ src/main/java/team9499/commitbody/domain/comment/exercise/controller/ExerciseCommentController.java
@@ -1,7 +1,13 @@
 package team9499.commitbody.domain.comment.exercise.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,10 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team9499.commitbody.domain.comment.exercise.service.ExerciseCommentService;
 import team9499.commitbody.global.authorization.domain.PrincipalDetails;
+import team9499.commitbody.global.payload.ErrorResponse;
 import team9499.commitbody.global.payload.SuccessResponse;
 
 import java.util.Map;
 
+@Tag(name = "운동 상세",description = "운동 상세페이지 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -22,8 +30,18 @@ public class ExerciseCommentController {
 
     private final ExerciseCommentService exerciseCommentService;
 
-    @PostMapping("/exercise-comment")
-    public ResponseEntity<?> saveExerciseComment(@RequestBody Map<String,String> commentRequest,
+    @Operation(summary = "운동 댓글 등록", description = "사용자는 해당 운동 목록에 댓글을 등록 가능합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\":true,\"message\":\"등록 성공\"}"))),
+            @ApiResponse(responseCode = "400_1", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "400_3", description = "BADREQUEST - 존재하지 않는 시용자 요청시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용자를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}")))})
+    @PostMapping("/comment-exercise")
+    public ResponseEntity<?> saveExerciseComment(@Parameter(schema = @Schema(type = "string", example = "{\"content\":\"string\"}"))@RequestBody Map<String,String> commentRequest,
                                                  @AuthenticationPrincipal PrincipalDetails principalDetails){
         Long memberId = principalDetails.getMember().getId();
         String content = commentRequest.get("content");
