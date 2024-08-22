f9d3c82f5292cb341d3659c36e7b9ebe28b82c21
CHISANW <keuye06380618@gmail.com>
2024-08-22 16:29:36 +0900

    refactor : 예외 발생시 클래스 리팩토링
    - 예외 발생시 예외 클래스를 data에 메시지를 담아 보냈지만 message안에 통합으로 보내도록 리팩토링

--- src/main/java/team9499/commitbody/global/payload/ErrorResponse.java
+++ src/main/java/team9499/commitbody/global/payload/ErrorResponse.java
@@ -1,6 +1,5 @@
 package team9499.commitbody.global.payload;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,19 +9,10 @@ public class ErrorResponse<T> {
 
     private boolean success;        // success : false
 
-    private String message;         // message : 실패
+    private T message;         // message : 실패
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private T data;                 // data : { id : 1l }
-
-    public ErrorResponse(boolean success, String message) {       // 추가적인 정보가 필요없는 생성자
+    public ErrorResponse(boolean success, T message) {       // 추가적인 정보가 필요없는 생성자
         this.success = success;
         this.message = message;
-    }
-
-    public ErrorResponse(boolean success, String message, T data) {       // 추가적인 정보가 필요로 하는 생성자
-        this.success = success;
-        this.message = message;
-        this.data = data;
     }
 }
