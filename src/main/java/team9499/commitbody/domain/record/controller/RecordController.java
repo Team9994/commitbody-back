49906e71a2421a7ca38330b2349c48275ed00eca
CHISANW <keuye06380618@gmail.com>
2024-08-25 15:27:21 +0900

    docs : 기록 조회 API Swagger 작성
    Ref : #55

--- src/main/java/team9499/commitbody/domain/record/controller/RecordController.java
+++ src/main/java/team9499/commitbody/domain/record/controller/RecordController.java
@@ -57,7 +57,7 @@ public class RecordController {
         return ResponseEntity.ok(new SuccessResponse<>(true,"루틴 성공",recordId));
     }
 
-    @Operation(summary = "기록 조회", description = "사용자가 완료한 기록의 대한 정보를 조회합니다.")
+    @Operation(summary = "기록 상세 조회", description = "사용자가 완료한 기록의 대한 상세 정보를 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
                     examples = @ExampleObject(value = "{\"success\":true,\"message\":\"조회 성공\",\"data\":{\"recordId\":1,\"recordName\":\"나의 첫 번째 기록\",\"startDate\":\"2024.08.14.(수)\",\"durationTime\":\"6:46~8:46\",\"duration\":120,\"recordVolume\":85,\"recordSets\":11,\"recordCalorie\":414,\"details\":[{\"recordDetailId\":1,\"exerciseId\":1,\"gifUrl\":\"https://example.com\",\"detailsReps\":12,\"detailsSets\":2,\"maxReps\":7,\"sets\":[{\"setId\":191,\"reps\":6},{\"setId\":192,\"reps\":7}]}]}}"))),
@@ -116,15 +116,22 @@ public class RecordController {
         return ResponseEntity.ok(new SuccessResponse<>(true,"삭제 성공"));
     }
 
+    @Operation(summary = "기록 조회", description = "사용자가 진행한 기록의 대해 모두 조회합니다. Default : size - 10, lastTime은 다음페이지 존재시 마지막 시간을 보냅니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\":true,\"message\":\"조회 성공\",\"data\":{\"dayRecordCount\":{\"1\":{\"day\":\"2024.08.01.(목)\",\"recordDays\":[{\"recordId\":56,\"recordName\":\"테스트\",\"durationTime\":\"2024.08.01.(목) · 18:37~20:37\"}]},\"3\":{\"day\":\"2024.08.03.(토)\",\"recordDays\":[{\"recordId\":58,\"recordName\":\"테스트\",\"durationTime\":\"2024.08.03.(토) · 18:37~20:37\"}]}},\"recordPage\":{\"hasNext\":false,\"records\":[{\"recordId\":57,\"recordName\":\"테스트\",\"durationTime\":\"2024.08.01.(목) · 18:37~20:37\",\"lastTime\":\"2024-08-01T20:37:32.71\"}]}}}"))),
+            @ApiResponse(responseCode = "400", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}")))})
     @GetMapping("/record")
     public ResponseEntity<?> get(@RequestParam(value = "lastTime",required = false) LocalDateTime lastTime,
-                                 @PageableDefault(size = 10) Pageable pageable,
+                                 @Parameter(example = "{\"size\":10}")@PageableDefault(size = 10) Pageable pageable,
                                  @AuthenticationPrincipal PrincipalDetails principalDetails){
         Long memberId = getMemberId(principalDetails);
         RecordMonthResponse recordForMember = recordService.getRecordForMember(memberId,lastTime,pageable);
         return ResponseEntity.ok(new SuccessResponse<>(true,"조회 성공",recordForMember));
     }
-
 
     private static Long getMemberId(PrincipalDetails principalDetails) {
         Long memberId = principalDetails.getMember().getId();
