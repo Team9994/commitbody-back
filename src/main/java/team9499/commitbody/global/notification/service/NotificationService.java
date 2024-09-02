dc10f583305cd3d7ef67574e632cd484f7b59b1f
CHISANW <keuye06380618@gmail.com>
2024-09-02 17:58:45 +0900

    feat : 알림 저장 및 전송 service 구현
    - 팔로우 요청시 알림 메세지를 생성하고 알림을 활성화한 사용자에게만 fcm 메시지가 발송 되도록 구현
    - 알림 데이터 저장과 푸쉬 알림 전달은 따로 이뤄줘도 된다 생각하여 비동기도 저장 및 발송하도록 구현
    
    Ref : #71

---
+++ src/main/java/team9499/commitbody/global/notification/service/NotificationService.java
@@ -0,0 +1,7 @@
+package team9499.commitbody.global.notification.service;
+
+public interface NotificationService {
+
+    void sendFollowing(Long followerId, Long followingId);
+
+}
---
+++ src/main/java/team9499/commitbody/global/notification/service/impl/NotificationServiceImpl.java
@@ -0,0 +1,52 @@
+package team9499.commitbody.global.notification.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team9499.commitbody.domain.Member.domain.Member;
+import team9499.commitbody.global.notification.domain.Notification;
+import team9499.commitbody.global.notification.domain.NotificationType;
+import team9499.commitbody.global.notification.repository.NotificationRepository;
+import team9499.commitbody.global.notification.service.FcmService;
+import team9499.commitbody.global.notification.service.NotificationService;
+import team9499.commitbody.global.redis.RedisService;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private final FcmService fcmService;
+    private final NotificationRepository notificationRepository;
+    private final RedisService redisService;
+
+    /**
+     * 비동기를 통한 알림 데이터 저장
+     */
+    @Async
+    public void asyncSave(Notification notification){
+        notificationRepository.save(notification);
+    }
+
+    /**
+     * 팔로우 관련 알림 전송 메서드
+     * @param followerId    발신자
+     * @param followingId   수신자
+     */
+    @Override
+    public void sendFollowing(Long followerId, Long followingId) {
+        Member followerMember = redisService.getMemberDto(String.valueOf(followerId)).get();        // 발신자
+        Member followingMember = redisService.getMemberDto(String.valueOf(followingId)).get();      // 수신자
+        String content = followerMember.getNickname()+"님이 회원님을 팔로우하기 시작했어요.";
+
+        Notification notification = Notification.of(content, NotificationType.FOLLOW, followingMember, followerMember);
+        asyncSave(notification);        // 비동기 저장
+
+        if (followingMember.isNotificationEnabled()) // 알림성정울 true로 한 상태 일때 알림 전송
+            fcmService.sendFollowingMessage(String.valueOf(followingMember.getId()),content);
+    }
+
+}
