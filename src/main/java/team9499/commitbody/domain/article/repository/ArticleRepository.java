6fad017219d53feabbd7acd15eded9710708cf66
CHISANW <keuye06380618@gmail.com>
2024-09-03 15:20:16 +0900

    feat : 게시글 repository 작성
    Ref : #76

---
+++ src/main/java/team9499/commitbody/domain/article/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package team9499.commitbody.domain.article.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team9499.commitbody.domain.article.domain.Article;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
