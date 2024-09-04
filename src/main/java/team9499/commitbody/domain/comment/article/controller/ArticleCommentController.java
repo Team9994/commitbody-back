0a6f2318321db14d30f3fedb88a40458d0f083be
CHISANW <keuye06380618@gmail.com>
2024-09-04 21:39:43 +0900

    docs : 게시글 댓글 작성 Swagger 작성
    Ref : #79

--- src/main/java/team9499/commitbody/domain/comment/article/controller/ArticleCommentController.java
+++ src/main/java/team9499/commitbody/domain/comment/article/controller/ArticleCommentController.java
@@ -1,12 +1,21 @@
 package team9499.commitbody.domain.comment.article.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import team9499.commitbody.domain.comment.article.dto.request.SaveArticleCommentRequest;
 import team9499.commitbody.domain.comment.article.service.ArticleCommentService;
 import team9499.commitbody.global.authorization.domain.PrincipalDetails;
+import team9499.commitbody.global.payload.ErrorResponse;
 import team9499.commitbody.global.payload.SuccessResponse;
 
 
@@ -17,8 +26,22 @@ public class ArticleCommentController {
 
     private final ArticleCommentService articleCommentService;
 
+    @Operation(summary = "운동 게시글 - 댓글 등록", description = "운동 게시글의 댓글을 작성하는 API 입니다. (replyNickname,parentId) 필요시에만 작성합니다.",tags = "게시글")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - 댓글 작성시 : 댓글 작성 성공, 대댓글 작성시 : 대댓글 작성 성공", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\":true,\"message\":\"댓글 작성 성공\"}"))),
+            @ApiResponse(responseCode = "400_1", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "400_2", description = "BADREQUEST - 값 입렵 검증", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":{\"필드명\": \"오류 내용\"}}"))),
+            @ApiResponse(responseCode = "400_3", description = "BADREQUEST - 데이터 미 존재시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"해당 정보를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "400_4", description = "BADREQUEST - 존재하지 않는 시용자 요청시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용자를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}")))})
     @PostMapping("/article/comment")
-    public ResponseEntity<?> save(@RequestBody SaveArticleCommentRequest request,
+    public ResponseEntity<?> save(@Valid @RequestBody SaveArticleCommentRequest request, BindingResult result,
                                   @AuthenticationPrincipal PrincipalDetails principalDetails){
         Long memberId = getMemberId(principalDetails);
         String commentType = articleCommentService.saveArticleComment(memberId, request.getArticleId(), request.getParentId(), request.getContent(), request.getReplyNickname());
