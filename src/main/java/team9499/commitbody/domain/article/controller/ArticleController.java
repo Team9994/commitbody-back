0f1f47ea132ca9701183bd24691d4a7bdff5e669
CHISANW <keuye06380618@gmail.com>
2024-09-04 13:52:53 +0900

    docs : 운동 게시글(운동 인증) Swagger 작성
    Ref : #76

--- src/main/java/team9499/commitbody/domain/article/controller/ArticleController.java
+++ src/main/java/team9499/commitbody/domain/article/controller/ArticleController.java
@@ -1,5 +1,12 @@
 package team9499.commitbody.domain.article.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -14,8 +21,10 @@ import org.springframework.web.multipart.MultipartFile;
 import team9499.commitbody.domain.article.dto.request.ArticleSaveRequest;
 import team9499.commitbody.domain.article.service.ArticleService;
 import team9499.commitbody.global.authorization.domain.PrincipalDetails;
+import team9499.commitbody.global.payload.ErrorResponse;
 import team9499.commitbody.global.payload.SuccessResponse;
 
+@Tag(name = "게시글",description = "게시글 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -23,6 +32,22 @@ public class ArticleController {
 
     private final ArticleService articleService;
 
+    @Operation(summary = "운동 게시글 등록", description = "운동 게시글을 작성하는 API 입니다. 관심운동 등록시에는 'articleCategory' 필드를 사용하지 않아도 됩니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - 게시글 작성 성공시 작성된 게시글 ID를 반환합니다.", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\":true,\"message\":\"등록 성공\",\"data\":1}"))),
+            @ApiResponse(responseCode = "400_1", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "400_2", description = "BADREQUEST - 값 입렵 검증", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":{\"필드명\": \"오류 내용\"}}"))),
+            @ApiResponse(responseCode = "400_3", description = "BADREQUEST - 파일 용량 초과(5MB 이하만 저장가능)",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"저장 가능한 용량을 초과 했습니다.\"}"))),
+            @ApiResponse(responseCode = "400_4", description = "BADREQUEST - 불가능한 이미지 파일 저장시(jpeg, jpg, png, gif 저장가능)",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"올바른 이미지 형식이 아닙니다.\"}"))),
+            @ApiResponse(responseCode = "400_5", description = "BADREQUEST - 존재하지 않는 시용자 요청시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용자를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}")))})
     @PostMapping(value = "/article",consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveArticle(@Valid @RequestPart("articleSaveRequest") ArticleSaveRequest request, BindingResult result,
                                          @RequestPart("file")MultipartFile file,
--- src/main/java/team9499/commitbody/domain/article/dto/request/ArticleSaveRequest.java
+++ src/main/java/team9499/commitbody/domain/article/dto/request/ArticleSaveRequest.java
@@ -1,5 +1,6 @@
 package team9499.commitbody.domain.article.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import team9499.commitbody.domain.article.domain.ArticleCategory;
@@ -7,19 +8,25 @@ import team9499.commitbody.domain.article.domain.ArticleType;
 import team9499.commitbody.domain.article.domain.Visibility;
 import team9499.commitbody.global.annotations.ValidEnum;
 
+@Schema(description = "운동 게시글 작성 Request")
 @Data
 public class ArticleSaveRequest {
 
+    @Schema(description = "게시글 제목")
     @NotBlank(message = "게시글 제목을 작성해주세요")
     private String title;
-
+    
+    @Schema(description = "게시글 내용 작성")
     @NotBlank(message = "게시글 내용을 작성해 주세요")
     private String content;
 
+    @Schema(description = "게시글 종류 [EXERCISE(운동 인증), INFO_QUESTION(정보 질문)] 을 사용합니다.")
     @ValidEnum(message = "게시글 종류를 작성해주세요",enumClass = ArticleType.class)
     private ArticleType articleType;
-    
+
+    @Schema(description = "게시글 종류가 INFO_QUESTION일 경우 사용합니다.")
     private ArticleCategory articleCategory;
 
+    @Schema(description = "게시글 작성시 게시글 공개범위를 선택합니다.[PUBLIC(전체 공개), FOLLOWERS_ONLY(팔로워만 공개), PRIVATE(비공개)]")
     private Visibility visibility;
 }
