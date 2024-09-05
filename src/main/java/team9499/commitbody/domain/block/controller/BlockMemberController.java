16431bfabccfa574cb31d7d7d274e12fc2f47ca1
CHISANW <keuye06380618@gmail.com>
2024-09-05 16:42:19 +0900

    feat : 사용자 차단 controller 구현
    - POST /api/v1/block/member 엔드포인트를 통해 사용자 차단 구현
    - 사용자 차단 SuccessResponse 객체및 "차단 성공", "차단 해제" 메세지를 를 반환합니다.
    
    Ref : #81

---
+++ src/main/java/team9499/commitbody/domain/block/controller/BlockMemberController.java
@@ -0,0 +1,32 @@
+package team9499.commitbody.domain.block.controller;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team9499.commitbody.domain.block.servcice.BlockMemberService;
+import team9499.commitbody.global.authorization.domain.PrincipalDetails;
+import team9499.commitbody.global.payload.SuccessResponse;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class BlockMemberController {
+
+    private final BlockMemberService blockMemberService;
+
+    @PostMapping("/block/member")
+    public ResponseEntity blockMember(@Parameter(schema = @Schema(example = "{\"blockedId\":1}")) @RequestBody Map<String,Long> reqeust,
+                                      @AuthenticationPrincipal PrincipalDetails principalDetails){
+        Long blockerId = principalDetails.getMember().getId();
+        String status = blockMemberService.blockMember(blockerId, reqeust.get("blockedId"));
+        return ResponseEntity.ok(new SuccessResponse<>(true,status));
+    }
+}
