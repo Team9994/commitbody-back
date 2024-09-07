ff4720a615fd817771613c00dd097a9bd023ec3e
CHISANW <keuye06380618@gmail.com>
2024-09-07 14:40:15 +0900

    docs : 프로플 페이지 - 운동 인증 게시글 조회 Swagger 작성
    Ref : #68

--- src/main/java/team9499/commitbody/domain/article/controller/ArticleController.java
+++ src/main/java/team9499/commitbody/domain/article/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package team9499.commitbody.domain.article.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -25,7 +26,7 @@ import team9499.commitbody.global.authorization.domain.PrincipalDetails;
 import team9499.commitbody.global.payload.ErrorResponse;
 import team9499.commitbody.global.payload.SuccessResponse;
 
-@Tag(name = "게시글",description = "게시글 관련 API")
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -33,6 +34,7 @@ public class ArticleController {
 
     private final ArticleService articleService;
 
+    @Tag(name = "게시글",description = "게시글 관련 API")
     @Operation(summary = "운동 게시글 등록", description = "운동 게시글을 작성하는 API 입니다. 관심운동 등록시에는 'articleCategory' 필드를 사용하지 않아도 됩니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK - 게시글 작성 성공시 작성된 게시글 ID를 반환합니다.", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
@@ -58,11 +60,19 @@ public class ArticleController {
         return ResponseEntity.ok(new SuccessResponse<>(true,"둥록 성공",articleId));
     }
 
+    @Operation(summary = "프로필 페이지 - 운동 인증 게시글 조회", description = "프로필 사용자가 작성한 운동인증 게시글을 조회합니다. Default Size = 12",tags = "프로필")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\": true, \"message\": \"조회 성공\", \"data\": {\"hasNext\": false, \"articles\": [{\"articleId\": 1, \"imageUrl\": \"https://d12ryzjapybmlj.cloudfront.net/images/7280c2c8-8b6b-4eeb-a2e7-55d15162fb06.png\"}]}}"))),
+            @ApiResponse(responseCode = "400", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}")))})
     @GetMapping("/my-page/exercise/{nickname}")
     public ResponseEntity<?> getAllExerciseArticle(@PathVariable("nickname") String nickname,
                                                    @RequestParam(value = "lastId",required = false) Long lastId,
                                                    @AuthenticationPrincipal PrincipalDetails principalDetails,
-                                                   @PageableDefault(size = 12) Pageable pageable){
+                                                   @Parameter(example = "{\"size\":12}") @PageableDefault(size = 12) Pageable pageable){
         String loginNickname = principalDetails.getMember().getNickname();
         ExerciseArticleResponse articles = articleService.getAllExerciseArticle(loginNickname, nickname, lastId, pageable);
         return ResponseEntity.ok(new SuccessResponse<>(true,"조회 성공",articles));
