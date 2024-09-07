3cec44a9d5ab30a4007c94cdb94ab0a4c6b31319
CHISANW <keuye06380618@gmail.com>
2024-09-07 14:46:57 +0900

    docs : 사용차단 API Swagger 작성
    Ref : #81

--- src/main/java/team9499/commitbody/domain/block/controller/BlockMemberController.java
+++ src/main/java/team9499/commitbody/domain/block/controller/BlockMemberController.java
@@ -1,7 +1,12 @@
 package team9499.commitbody.domain.block.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +19,7 @@ import team9499.commitbody.domain.block.event.CancelBlockMemberEvent;
 import team9499.commitbody.domain.block.event.ElsBlockMemberEvent;
 import team9499.commitbody.domain.block.servcice.BlockMemberService;
 import team9499.commitbody.global.authorization.domain.PrincipalDetails;
+import team9499.commitbody.global.payload.ErrorResponse;
 import team9499.commitbody.global.payload.SuccessResponse;
 
 import java.util.Map;
@@ -26,6 +32,16 @@ public class BlockMemberController {
     private final BlockMemberService blockMemberService;
     private final ApplicationEventPublisher eventPublisher;
 
+    @Operation(summary = "사용자 차단", description = "하나의 API를 통해 차단/해제 요청을 수행합니다.",tags = "팔로워")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - 각 요청 응답에 맞게 [차단 해제, 차단 성공]을 반환 합니다.", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\": true, \"message\": \"차단 성공\"}"))),
+            @ApiResponse(responseCode = "400_1", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "400_2", description = "BADREQUEST - 사용자 존재하지 않을시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용자를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}")))})
     @PostMapping("/block/member")
     public ResponseEntity blockMember(@Parameter(schema = @Schema(example = "{\"blockedId\":1}")) @RequestBody Map<String,Long> reqeust,
                                       @AuthenticationPrincipal PrincipalDetails principalDetails){
