7f43722dadd5368797d57c5f05fcb9570cf58e31
CHISANW <keuye06380618@gmail.com>
2024-09-09 21:30:06 +0900

    feat : 게시글 댓글 조회 Service 구현
    - 게시글의 데이터를 조회하기전 조회하는 사용자가 조회하는 사용자를 차단했는지 여부를 판단후 차단된 사용자라면 예외를 발생하도록 하며,
    차단한 사용자의 게시글의 접근시에는 Member 객체안에  blockStatus(boolean) 을통해 차단여부 확인
    
    Ref : #76

--- src/main/java/team9499/commitbody/domain/article/service/ArticleService.java
+++ src/main/java/team9499/commitbody/domain/article/service/ArticleService.java
@@ -5,11 +5,14 @@ import org.springframework.web.multipart.MultipartFile;
 import team9499.commitbody.domain.article.domain.ArticleCategory;
 import team9499.commitbody.domain.article.domain.ArticleType;
 import team9499.commitbody.domain.article.domain.Visibility;
+import team9499.commitbody.domain.article.dto.ArticleDto;
 import team9499.commitbody.domain.article.dto.response.ProfileArticleResponse;
 
 public interface ArticleService {
 
     Long saveArticle(Long memberId, String title, String content, ArticleType articleType, ArticleCategory articleCategory, Visibility visibility, MultipartFile file);
+
+    ArticleDto getDetailArticle(Long memberId, Long articleId);
 
     ProfileArticleResponse getAllProfileArticle(Long loginMemberId, Long findMemberId, ArticleType articleType, Long lastId, Pageable pageable);
 }
