19b6f57a4700feae9040bb4a0d15988ec1bd1410
CHISANW <keuye06380618@gmail.com>
2024-09-11 21:53:12 +0900

    doc : 게시글 좋아요 API Swaggeer 작성
    Ref : #76

--- src/main/java/team9499/commitbody/domain/like/exercise/controller/ExerciseCommentLikeController.java
+++ src/main/java/team9499/commitbody/domain/like/exercise/controller/ExerciseCommentLikeController.java
@@ -43,9 +43,20 @@ public class ExerciseCommentLikeController {
         return ResponseEntity.ok(new SuccessResponse<>(true,commentLikeStatus));
     }
 
-
+    @Operation(summary = "게시글 좋아요", description = "게시글의 대해서 좋아요 등록/해제 기능을 합니다.",tags = "게시글")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - 좋아요 성공시[\"등록\"], 좋아요 해제시[\"해제\"]", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\": true, \"message\": \"등록\"}"))),
+            @ApiResponse(responseCode = "400_1", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "400_2", description = "BADREQUEST - 존재하지 않는 시용자 요청시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용자를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "400_3", description = "BADREQUEST - 존재하지 않는 게시글 요청시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"해당 정보를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}")))})
     @PostMapping("/article/like")
-    public ResponseEntity<?> articleLike(@RequestBody Map<String, Long> articleLikeRequest,
+    public ResponseEntity<?> articleLike(@Parameter(schema = @Schema(example = "{\"articleId\": 1}"))@RequestBody Map<String, Long> articleLikeRequest,
                                          @AuthenticationPrincipal PrincipalDetails principalDetails){
         Long memberId = principalDetails.getMember().getId();
         String articleLike = exerciseCommentLikeService.articleLike(articleLikeRequest.get("articleId"), memberId);
