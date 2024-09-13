ddedc43d2ff3cf3c0abc69fc60fe8283d811b16d
CHISANW <keuye06380618@gmail.com>
2024-09-14 05:10:47 +0900

    docs : 댓글 수정/삭제 Swaager 작성 및 수정
    - 댓글 삭제/수정의 대한 Swaager 작성
    - 게시글 관련 명칭을 운동 게시글 -> 게시글 로변경
    
    Ref : #79

--- src/main/java/team9499/commitbody/domain/comment/article/controller/ArticleCommentController.java
+++ src/main/java/team9499/commitbody/domain/comment/article/controller/ArticleCommentController.java
@@ -33,7 +33,7 @@ public class ArticleCommentController {
 
     private final ArticleCommentService articleCommentService;
 
-    @Operation(summary = "운동 게시글 - 댓글 등록", description = "운동 게시글의 댓글을 작성하는 API 입니다. (replyNickname,parentId) 필요시에만 작성합니다.",tags = "게시글")
+    @Operation(summary = "게시글 - 댓글 등록", description = "운동 게시글의 댓글을 작성하는 API 입니다. (replyNickname,parentId) 필요시에만 작성합니다.",tags = "게시글")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK - 댓글 작성시 : 댓글 작성 성공, 대댓글 작성시 : 대댓글 작성 성공", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
                     examples = @ExampleObject(value = "{\"success\":true,\"message\":\"댓글 작성 성공\"}"))),
@@ -55,10 +55,22 @@ public class ArticleCommentController {
 
         return ResponseEntity.ok(new SuccessResponse<>(true,commentType));
     }
-
+    
+    @Operation(summary = "게시글 - 대/댓글 수정", description = "작성자만 작성한 댓글을 수정 가능합니다.",tags = "게시글")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\": true, \"message\": \"댓글 수정 성공\"}"))),
+            @ApiResponse(responseCode = "400_1", description = "BADREQUEST - 데이터 미 존재시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"해당 정보를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "400_2", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}"))),
+            @ApiResponse(responseCode = "403", description = "타 사용자가 삭제시", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"작성자만 이용할 수 있습니다.\"}")))})
     @PutMapping("/article/comment/{commentId}")
     public ResponseEntity<?> updateComment(@PathVariable("commentId") Long commentId,
-                                           @RequestBody Map<String,String> updateComment,
+                                           @Parameter(schema = @Schema(example = "{\"content\":\"댓글 수정 내용\"}"))@RequestBody Map<String,String> updateComment,
                                            @AuthenticationPrincipal PrincipalDetails principalDetails){
         String content = updateComment.get("content");
         Long memberId = getMemberId(principalDetails);
@@ -66,6 +78,18 @@ public class ArticleCommentController {
         return ResponseEntity.ok(new SuccessResponse<>(true,"댓글 수정 성공"));
     }
 
+    @Operation(summary = "게시글 - 대/댓글 삭제", description = "작성자만 작성한 댓글을 삭제 가능합니다.",tags = "게시글")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
+                    examples = @ExampleObject(value = "{\"success\": true, \"message\": \"댓글 수정 성공\"}"))),
+            @ApiResponse(responseCode = "400_1", description = "BADREQUEST - 데이터 미 존재시",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"해당 정보를 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "400_2", description = "BADREQUEST - 사용 불가 토큰",content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"사용할 수 없는 토큰입니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"토큰이 존재하지 않습니다.\"}"))),
+            @ApiResponse(responseCode = "403", description = "타 사용자가 삭제시", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(value = "{\"success\" : false,\"message\":\"작성자만 이용할 수 있습니다.\"}")))})
     @DeleteMapping("/article/comment/{commentId}")
     public ResponseEntity<?> deleteComment(@PathVariable("commentId") Long commentId,
                                            @AuthenticationPrincipal PrincipalDetails principalDetails){
@@ -74,7 +98,7 @@ public class ArticleCommentController {
         return ResponseEntity.ok(new SuccessResponse<>(true,"삭제 성공"));
                                            }
 
-    @Operation(summary = "운동 게시글 - 댓글 조회", description = "운동 게시글의 작성된 댓글을 조회합니다. sortOrder:[RECENT,LIKE] 이며, LIKE 정렬시 마지막 조회 데이터의 likeCount가 0일경우 lastLikeCount와 lastId를 사용하며, 0이 아닐시에는 lastLikeCount만 사용합니다.)",tags = "게시글")
+    @Operation(summary = "게시글 - 댓글 조회", description = "운동 게시글의 작성된 댓글을 조회합니다. sortOrder:[RECENT,LIKE] 이며, LIKE 정렬시 마지막 조회 데이터의 likeCount가 0일경우 lastLikeCount와 lastId를 사용하며, 0이 아닐시에는 lastLikeCount만 사용합니다.)",tags = "게시글")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
                     examples = @ExampleObject(value = "{\"success\": true, \"message\": \"댓글 조회\", \"data\": {\"totalCount\" : 2,\"hasNext\": true, \"comments\": [{\"commentId\": 82, \"content\": \"댓글\", \"nickname\": \"세번쨰닉네임\", \"profile\": \"https://d12ryzjapybmlj.cloudfront.net/default.PNG\", \"time\": \"25분 전\", \"likeCount\": 0, \"replyCount\": 0, \"writer\": false}]}}"))),
@@ -95,7 +119,7 @@ public class ArticleCommentController {
         return ResponseEntity.ok(new SuccessResponse<>(true,"댓글 조회",comments));
     }
 
-    @Operation(summary = "운동 게시글 - 대댓글 조회", description = "운동 게시글의 작성된 댓글의 대댓글을 조회합니다.",tags = "게시글")
+    @Operation(summary = "게시글 - 대댓글 조회", description = "운동 게시글의 작성된 댓글의 대댓글을 조회합니다.",tags = "게시글")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SuccessResponse.class),
                     examples = @ExampleObject(value = "{\"success\": true, \"message\": \"대댓글 조회\", \"data\": {\"hasNext\": false, \"comments\": [{\"commentId\": 26, \"content\": \"대댓글?\", \"nickname\": \"테스트닉네임\", \"profile\": \"https://d12ryzjapybmlj.cloudfront.net/default.PNG\", \"time\": \"5시간 전\", \"likeCount\": 0, \"writer\": true}]}}"))),
