8d2c59ba53ce03067b15d591d63401ec688eff62
CHISANW <keuye06380618@gmail.com>
2024-10-12 17:35:44 +0900

    refactor : 운동 댓글 API 리팩토링
    - 운동 댓글 생성시 of 팩터리 메서드의 false 에서 true로 변경
    - 운동 댓글 조회시 좋아요 상태 체크시 null 조건을 추가하여 null 이아닐때 진행하도록 변경
    - service 단에서 사용자를 조회를 DB에서 조회했으나 Redis에서 사용자를 조회하도록 변경
    
    Ref : #115

--- src/main/java/team9499/commitbody/domain/comment/exercise/service/impl/ExerciseCommentServiceImpl.java
+++ src/main/java/team9499/commitbody/domain/comment/exercise/service/impl/ExerciseCommentServiceImpl.java
@@ -1,13 +1,11 @@
 package team9499.commitbody.domain.comment.exercise.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team9499.commitbody.domain.Member.domain.Member;
-import team9499.commitbody.domain.Member.repository.MemberRepository;
 import team9499.commitbody.domain.comment.exercise.domain.ExerciseComment;
 import team9499.commitbody.domain.comment.exercise.dto.ExerciseCommentDto;
 import team9499.commitbody.domain.comment.exercise.dto.response.ExerciseCommentResponse;
@@ -17,24 +15,24 @@ import team9499.commitbody.domain.exercise.repository.CustomExerciseRepository;
 import team9499.commitbody.domain.exercise.repository.ExerciseRepository;
 import team9499.commitbody.global.Exception.InvalidUsageException;
 import team9499.commitbody.global.Exception.NoSuchException;
+import team9499.commitbody.global.redis.RedisService;
 
 import static team9499.commitbody.global.Exception.ExceptionStatus.*;
 import static team9499.commitbody.global.Exception.ExceptionType.*;
 
-@Slf4j
 @Service
 @Transactional(transactionManager = "dataTransactionManager")
 @RequiredArgsConstructor
 public class ExerciseCommentServiceImpl implements ExerciseCommentService {
 
+    private final RedisService redisService;
     private final ExerciseCommentRepository exerciseCommentRepository;
     private final ExerciseRepository exerciseRepository;
     private final CustomExerciseRepository customExerciseRepository;
-    private final MemberRepository memberRepository;
 
     @Override
     public void saveExerciseComment(Long memberId, Long exerciseId, String source, String comment) {
-        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NoSuchException(BAD_REQUEST, No_SUCH_MEMBER));
+        Member member = redisService.getMemberDto(memberId.toString()).get();
         Object exercise;
         if (source.equals("default")){
             exercise = exerciseRepository.findById(exerciseId).orElseThrow(() -> new NoSuchException(BAD_REQUEST, NO_SUCH_DATA));
