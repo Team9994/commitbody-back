a29b5dc06b1dfc599bb63adf3726bbf4499b7a4c
CHISANW <keuye06380618@gmail.com>
2024-10-14 21:14:21 +0900

    refactor : 팔로워 API 리팩토링
    - 팔러워/팔로잉 목록 조회시 조회한 LIST가 NULL이 아닐 경우의 매핑하도록 조건 추가
    - FollowResponse의 제네릭타입을 제거후 명시적 타입 적용
    
    Ref : #123

--- src/main/java/team9499/commitbody/domain/follow/dto/response/FollowResponse.java
+++ src/main/java/team9499/commitbody/domain/follow/dto/response/FollowResponse.java
@@ -3,14 +3,15 @@ package team9499.commitbody.domain.follow.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import team9499.commitbody.domain.follow.dto.FollowDto;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class FollowResponse<T> {
+public class FollowResponse {
 
     private boolean hasNext;
-    private List<T> follows;
+    private List<FollowDto> follows;
 }
