5d28877c333a0671973d2219cde0d098144455e6
CHISANW <keuye06380618@gmail.com>
2024-11-04 17:51:33 +0900

    refactor: Elastic repository 리팩토링
    - 차단 사용자의 dao단의 @Repository 추가

--- src/main/java/team9499/commitbody/domain/block/repository/ElsBlockMemberRepository.java
+++ src/main/java/team9499/commitbody/domain/block/repository/ElsBlockMemberRepository.java
@@ -1,7 +1,9 @@
 package team9499.commitbody.domain.block.repository;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 import team9499.commitbody.domain.block.domain.BlockMemberDoc;
 
+@Repository
 public interface ElsBlockMemberRepository extends ElasticsearchRepository<BlockMemberDoc,String> {
 }
