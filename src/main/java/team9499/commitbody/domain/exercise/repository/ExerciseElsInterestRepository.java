db4ecc5ad685a8069c84fcbe523535c404ee8c37
CHISANW <keuye06380618@gmail.com>
2024-11-04 18:04:05 +0900

    refactor: 관심 엘라스틱 리팩토링
    - ExerciseElsInterestRepository의 @Repository 에노테이션 추가

--- src/main/java/team9499/commitbody/domain/exercise/repository/ExerciseElsInterestRepository.java
+++ src/main/java/team9499/commitbody/domain/exercise/repository/ExerciseElsInterestRepository.java
@@ -1,7 +1,9 @@
 package team9499.commitbody.domain.exercise.repository;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 import team9499.commitbody.domain.exercise.domain.ExerciseInterestDoc;
 
+@Repository
 public interface ExerciseElsInterestRepository extends ElasticsearchRepository<ExerciseInterestDoc, Long> {
 }
