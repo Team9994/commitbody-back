f63800f5e4024c0491b5fb70241734a1d999e5df
CHISANW <keuye06380618@gmail.com>
2024-11-15 17:38:03 +0900

    feat : 특수 기호 매직 상수 클래스 생성

---
+++ src/main/java/team9499/commitbody/global/constants/Delimiter.java
@@ -0,0 +1,7 @@
+package team9499.commitbody.global.constants;
+
+public class Delimiter {
+
+    public final static String DASH = "-";
+    public final static String HYPHEN = "_";
+    public final static String STAR = "*";}
