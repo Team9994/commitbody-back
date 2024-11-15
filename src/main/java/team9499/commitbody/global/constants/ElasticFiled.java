009554038518b79fb4c4108c9f58cd6bb8e16e0b
CHISANW <keuye06380618@gmail.com>
2024-11-15 17:38:28 +0900

    feat : Elastic에서 사용될 변수명을 매직상수 클래스 생성
    - 엘라스틱 사용시 하드코딩을 하지않기 위해 매직상수 사용

---
+++ src/main/java/team9499/commitbody/global/constants/ElasticFiled.java
@@ -0,0 +1,41 @@
+package team9499.commitbody.global.constants;
+
+public class ElasticFiled {
+
+    // COMMON
+    public static final String MEMBER_ID = "memberId";  // 멤버 필드
+    public static final String STATUS = "status";   // 상태
+    public static final String _SCORE = "_score";
+    public static final String _SOURCE = "_source";
+    public static final String CUSTOM_ = "custom_";
+    public static final String DEFAULT_ = "default_";
+    public static final String DEFAULT = "default";
+    public static final String ID = "id";
+
+    public static final String ADD = "등록";
+    public static final String CANCEL = "해제";
+
+    
+    public static final String INTEREST_INDEX = "exercise_interest_index";
+
+
+    // EXERCISE DOC
+    public static final String EXERCISE_INDEX = "exercise_index";
+    public static final String EXERCISE_ID = "exerciseId";
+    public static final String EXERCISE_NAME = "exerciseName";
+    public static final String EXERCISE_GIF = "gifUrl";
+    public static final String EXERCISE_TARGET = "exerciseTarget";
+    public static final String EXERCISE_TYPE = "exerciseType";
+    public static final String EXERCISE_EQUIPMENT = "exerciseEquipment";
+    public static final String EXERCISE_INTEREST = "interest";
+    public static final String SOURCE = "source";
+
+    public static final String NO_IMAGE = "등록된 이미지 파일이 없습니다.";
+    public static final String WEIGHT_AND_LEP = "무게와 횟수";
+
+    // Script
+    public static final String CTX_WITH_DRAW = "ctx._source.withDraw = params.writDraw";
+    public static final String PAINLESS = "painless";
+    public static final String WRIT_DRAW = "writDraw";
+
+}
