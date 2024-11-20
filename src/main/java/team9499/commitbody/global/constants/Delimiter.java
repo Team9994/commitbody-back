ad11f06fb84cff04fb1e2c49f2e2ba7feb984361
CHISANW <keuye06380618@gmail.com>
2024-11-20 16:06:37 +0900

    feat : 상수값 추가
    - . 을 사용하기 위해 상수값 추가

--- src/main/java/team9499/commitbody/global/constants/Delimiter.java
+++ src/main/java/team9499/commitbody/global/constants/Delimiter.java
@@ -4,4 +4,7 @@ public class Delimiter {
 
     public final static String DASH = "-";
     public final static String HYPHEN = "_";
-    public final static String STAR = "*";}
+    public final static String STAR = "*";
+    public final static String COMMA = ".";
+    public final static String STRING_EMPTY = "";
+}
