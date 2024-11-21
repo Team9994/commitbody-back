3da7027eeb5d1e955e269bbceec90dfb1570fe5a
CHISANW <keuye06380618@gmail.com>
2024-11-21 16:49:21 +0900

    refactor : MemberServiceImpl 리팩토링
    - 주요 조회 기능 메서드가 지나치게 길고 복잡하여 작은 메서드로 분리
    - 메서드 분리를 통해 코드 가독성과 유지보수성을 향상
    - 공통 로직을 재사용하도록 정리하여 중복 제거

--- src/main/java/team9499/commitbody/domain/Member/service/impl/MemberServiceImpl.java
+++ src/main/java/team9499/commitbody/domain/Member/service/impl/MemberServiceImpl.java
@@ -27,6 +27,11 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
+    private final static String NOTIFICATION = "알림 수신";
+    private final static String NOT_NOTIFICATION = "알림 미수신";
+    private final static String MY_PAGE = "myPage";
+    private final static String THEIR_PAGE = "theirPage";
+
     private final MemberRepository memberRepository;
     private final FollowRepository followRepository;
     private final BlockMemberService blockMemberService;
@@ -35,65 +40,47 @@ public class MemberServiceImpl implements MemberService {
     private final RedisService redisService;
     private final S3Service s3Service;
 
-
     /**
      * 마이페이지 조회 API
-     * @param memberId  현재 로그인한 사용자 정보
+     *
+     * @param memberId 현재 로그인한 사용자 정보
      */
     @Override
-    public MemberMyPageResponse getMyPage(Long memberId,String nickname) {
-        Member member = memberRepository.findByNickname(nickname).filter(m -> !m.isWithdrawn()).orElseThrow(() -> new NoSuchException(ExceptionStatus.BAD_REQUEST, ExceptionType.No_SUCH_MEMBER));
-
-        // 마이페이지 존재시 차단 상태 유무체크 상대방이 사용자를 차단된 상태일경우 예외 발생 
-        boolean blockStatus = blockMemberService.checkBlock(member.getId(),memberId);   
-
-        int countFollower = (int) followRepository.getCountFollower(member.getId());
-        int countFollowing = (int) followRepository.getCountFollowing(member.getId());
-        MemberMyPageResponse.MemberMyPageResponseBuilder memberMyPageResponseBuilder =
-                MemberMyPageResponse.builder().nickname(member.getNickname()).profile(member.getProfile()).followerCount(countFollower).followingCount((countFollowing)).blockStatus(blockStatus);
-        
-        //현재 마이페이지로 접근 시
-        if (member.getId().equals(memberId)) {
-            memberMyPageResponseBuilder.memberId(memberId).pageType("myPage");
-        }else{  // 상대방 페이지 접근시
-            FollowType followStatus = followRepository.followStatus(memberId, member.getId());  // 상대방과 팔로우 관계를 검사
-            memberMyPageResponseBuilder.followStatus(followStatus).accountStatus(member.getAccountStatus()).memberId(member.getId()).pageType("theirPage");
-        }
-        return memberMyPageResponseBuilder.build();
+    public MemberMyPageResponse getMyPage(Long memberId, String nickname) {
+        Member member = filterWithDrawnMember(nickname);
+        MemberMyPageResponse.MemberMyPageResponseBuilder myPageBuilder = getMemberMyPageResponseBuilder(memberId, member);
+        return handleMyPageResponse(memberId, member, myPageBuilder);
     }
-
 
     /**
-     *  프로필 업데이트
-     * @param memberId  수정할 사용자 ID
-     * @param nickname  닉네임
-     * @param gender    성별
-     * @param birthDay  생년월일
-     * @param height    키
-     * @param weight    몸무게
-     * @param boneMineralDensity  골근격량
+     * 프로필 업데이트
+     *
+     * @param memberId           수정할 사용자 ID
+     * @param nickname           닉네임
+     * @param gender             성별
+     * @param birthDay           생년월일
+     * @param height             키
+     * @param weight             몸무게
+     * @param boneMineralDensity 골근격량
      * @param bodyFatPercentage  체지방량
-     * @param deleteProfile 기본 프로필 변경 여부
-     * @param file
+     * @param deleteProfile      기본 프로필 변경 여부
      */
     @Override
-    public void updateProfile(Long memberId, String nickname, Gender gender, LocalDate birthDay, Float height, Float weight, Float boneMineralDensity,
-                              Float bodyFatPercentage, boolean deleteProfile,MultipartFile file) {
-        Member member = redisService.getMemberDto(memberId.toString()).get();
+    public void updateProfile(Long memberId, String nickname, Gender gender, LocalDate birthDay, Float height, Float weight,
+                              Float boneMineralDensity, Float bodyFatPercentage, boolean deleteProfile, MultipartFile file) {
+        Member member = getMember(memberId);
         String beforeNickname = member.getNickname();   // 변경하기전 닉네임
-        String profile = s3Service.updateProfile(file, member.getProfile(),deleteProfile);
-        member.updateProfile(nickname,gender,birthDay,height,weight,boneMineralDensity,bodyFatPercentage,profile);
-        
-        redisService.updateMember(String.valueOf(memberId),member); // 레디스 회원 정보 업데이트
-
-        memberDocService.updateMemberDocAsync(String.valueOf(memberId),nickname,profile);   // 사용자 인덱스 업데이트
-        elsArticleService.updateWriterAsync(beforeNickname,nickname);   // 게시글의 작성된 사용자 닉네임 업데이트
+        String profile = s3Service.updateProfile(file, member.getProfile(), deleteProfile);
+        member.updateProfile(nickname, gender, birthDay, height, weight, boneMineralDensity, bodyFatPercentage, profile);
+        handleUpdateProfile(memberId, nickname, member, profile, beforeNickname);
     }
+
 
     /**
      * 현자 사용자의 알림 유뮤를 조회합니다.
+     *
      * @param memberId 로그인한 사용자 아이디
-     * @return  true 알림 수신 false 알림 미수신
+     * @return true 알림 수신 false 알림 미수신
      */
     @Override
     public boolean getNotification(Long memberId) {
@@ -103,18 +90,52 @@ public class MemberServiceImpl implements MemberService {
 
     /**
      * 사용자가 알림 수신 여부를 선택합니다.
-     * @param memberId  현재 로그인한 사용자 ID
-     * @return  알림 수신 , 알림 미수신
+     *
+     * @param memberId 현재 로그인한 사용자 ID
+     * @return 알림 수신 , 알림 미수신
      */
     @Override
     public String updateNotification(Long memberId) {
-        Member member = redisService.getMemberDto(memberId.toString()).get();
-
+        Member member = getMember(memberId);
         boolean notificationEnabled = member.isNotificationEnabled();
         member.updateNotification(!notificationEnabled);        // 알림 수신 유무 반대로 저장
-        redisService.updateMember(String.valueOf(member),member);       // 레디시의 정보 업데이트
+        redisService.updateMember(String.valueOf(member), member);       // 레디시의 정보 업데이트
+        return notificationEnabled ? NOT_NOTIFICATION : NOTIFICATION;
+    }
 
-        return notificationEnabled ? "알림 미수신" : "알림 수신";
+    private Member filterWithDrawnMember(String nickname) {
+        return memberRepository.findByNickname(nickname).filter(m -> !m.isWithdrawn())
+                .orElseThrow(() -> new NoSuchException(ExceptionStatus.BAD_REQUEST, ExceptionType.No_SUCH_MEMBER));
+    }
+
+    private MemberMyPageResponse.MemberMyPageResponseBuilder getMemberMyPageResponseBuilder(Long memberId, Member member) {
+        boolean blockStatus = blockMemberService.checkBlock(member.getId(), memberId);
+
+        int countFollower = (int) followRepository.getCountFollower(member.getId());
+        int countFollowing = (int) followRepository.getCountFollowing(member.getId());
+        return MemberMyPageResponse.builder().nickname(member.getNickname()).profile(member.getProfile())
+                .followerCount(countFollower).followingCount((countFollowing)).blockStatus(blockStatus);
+    }
+
+    private MemberMyPageResponse handleMyPageResponse(Long memberId, Member member,
+                                                      MemberMyPageResponse.MemberMyPageResponseBuilder myPageBuilder) {
+        if (member.getId().equals(memberId)) {
+            return myPageBuilder.memberId(memberId).pageType(MY_PAGE).build();
+        }
+        FollowType followStatus = followRepository.followStatus(memberId, member.getId());  // 상대방과 팔로우 관계를 검사
+        return myPageBuilder.followStatus(followStatus).accountStatus(member.getAccountStatus())
+                .memberId(member.getId()).pageType(THEIR_PAGE).build();
+    }
+
+    private Member getMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NoSuchException(ExceptionStatus.BAD_REQUEST, ExceptionType.No_SUCH_MEMBER));
+    }
+
+    private void handleUpdateProfile(Long memberId, String nickname, Member member, String profile, String beforeNickname) {
+        redisService.updateMember(String.valueOf(memberId), member); // 레디스 회원 정보 업데이트
+        memberDocService.updateMemberDocAsync(String.valueOf(memberId), nickname, profile);   // 사용자 인덱스 업데이트
+        elsArticleService.updateWriterAsync(beforeNickname, nickname);   // 게시글의 작성된 사용자 닉네임 업데이트
     }
 
 }
