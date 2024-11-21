8548f27b994057091fa50f311f8b4097fecd441b
CHISANW <keuye06380618@gmail.com>
2024-11-21 17:03:43 +0900

    refactor : JwtUtils 리팩토링
    - 주요 조회 기능 메서드가 지나치게 길고 복잡하여 작은 메서드로 분리
    - 메서드 분리를 통해 코드 가독성과 유지보수성을 향상
    - 매직상수를 통해 하드코딩 값 제거
    - 메서드명 수정

--- src/main/java/team9499/commitbody/global/utils/JwtUtils.java
+++ src/main/java/team9499/commitbody/global/utils/JwtUtils.java
@@ -1,6 +1,5 @@
 package team9499.commitbody.global.utils;
 
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.AlgorithmMismatchException;
@@ -24,30 +23,30 @@ import static team9499.commitbody.global.Exception.ExceptionType.*;
 @Component
 public class JwtUtils {
 
+    private final static String MEMBER_ID = "memberId";
+    private final static String ACCESS_TOKEN = "accessToken";
+    private final static String REFRESH_TOKEN = "refreshToken";
+
     @Value("${jwt.access}")
     private int ACCESS_TOKEN_EXPIRED;
-
     @Value("${jwt.refresh}")
     private int REFRESH_TOKEN_EXPIRED;
-
     @Value("${jwt.secret}")
     private String SECRET;
-
-    private final static String MEMBER_ID ="memberId";
 
     /**
      * AccessToken과 RefreshToken을 발급을 하는 메서드
      */
-    public Map<String,String> generateAuthTokens(MemberDto memberDto){
+    public Map<String, String> generateAuthTokens(MemberDto memberDto) {
         String accessToken = generateAccessToken(memberDto);
         String refreshToken = generateRefreshToken(memberDto);
-        return Map.of("accessToken", accessToken, "refreshToken", refreshToken);
+        return Map.of(ACCESS_TOKEN, accessToken, REFRESH_TOKEN, refreshToken);
     }
 
     /**
      * AccessToken 발급을 담당하는 메서드
      */
-    public  String generateAccessToken(MemberDto memberDto) {
+    public String generateAccessToken(MemberDto memberDto) {
         LocalDateTime now = LocalDateTime.now().plusHours(ACCESS_TOKEN_EXPIRED);
         return creatToken(memberDto, Timestamp.valueOf(now));
     }
@@ -55,30 +54,32 @@ public class JwtUtils {
     /**
      * RefreshToken 발급을 당당하는 메서드
      */
-    public  String generateRefreshToken(MemberDto memberDto) {
+    public String generateRefreshToken(MemberDto memberDto) {
         LocalDateTime now = LocalDateTime.now().plusMonths(REFRESH_TOKEN_EXPIRED);
         return creatToken(memberDto, Timestamp.valueOf(now));
     }
 
-    /*
-    실질적은 Token을 생성해주는 메서드
-     */
-    private String creatToken(MemberDto memberDto, Date expirationDate) {
-        return  JWT.create()
-                .withExpiresAt(expirationDate)
-                .withClaim(MEMBER_ID, String.valueOf(memberDto.getMemberId()))
-                .sign(Algorithm.HMAC512(SECRET));
-    }
-
-    public String accessTokenValid(String jwtToken){
+    public String accessTokenValid(String jwtToken) {
         try {
-            if (!tokenExpiration(jwtToken)) {
+            if (!isTokenExpired(jwtToken)) {
                 return verifyToken(jwtToken);
-            }else
-                throw new JwtTokenException(UNAUTHORIZED,TOKEN_EXPIRED);
+            }
+            throw new JwtTokenException(UNAUTHORIZED, TOKEN_EXPIRED);
         } catch (SignatureVerificationException e) {
             log.error("존재하지 않은 토큰 사용");
             throw new JwtTokenException(UNAUTHORIZED, TOKEN_NOT_FOUND);
+        }
+    }
+
+    /*
+    토큰의 대한 시간 검증 (true : 토큰 만료 , false : 토큰 유효)
+     */
+    public Boolean isTokenExpired(String token) {
+        try {
+            Date expired = JWT.decode(token).getExpiresAt();
+            return expired != null && expired.before(new Date());
+        } catch (JWTDecodeException e) {
+            throw new JwtTokenException(BAD_REQUEST, INVALID_TOKEN_MESSAGE);
         }
     }
 
@@ -87,26 +88,26 @@ public class JwtUtils {
      */
     public String verifyToken(String tokenValue) {
         try {
-            return JWT.require(Algorithm.HMAC512(SECRET)).build()
-                    .verify(tokenValue)
-                    .getClaim(MEMBER_ID)
-                    .asString();
-        }catch (AlgorithmMismatchException e){
-            throw new JwtTokenException(BAD_REQUEST,TOKEN_NOT_FOUND);
+            return jwtVerify(tokenValue);
+        } catch (AlgorithmMismatchException e) {
+            throw new JwtTokenException(BAD_REQUEST, TOKEN_NOT_FOUND);
         }
-
     }
 
     /*
-    토큰의 대한 시간 검증 (true : 토큰 만료 , false : 토큰 유효)
-     */
-    public Boolean tokenExpiration(String token) {
-        try {
-            Date expired = JWT.decode(token).getExpiresAt();
-            return expired != null && expired.before(new Date());
-        }catch (JWTDecodeException e){
-            throw new JwtTokenException(BAD_REQUEST,INVALID_TOKEN_MESSAGE);
-        }
+     실질적은 Token을 생성해주는 메서드
+    */
+    private String creatToken(MemberDto memberDto, Date expirationDate) {
+        return JWT.create()
+                .withExpiresAt(expirationDate)
+                .withClaim(MEMBER_ID, String.valueOf(memberDto.getMemberId()))
+                .sign(Algorithm.HMAC512(SECRET));
+    }
 
+    private String jwtVerify(String tokenValue) {
+        return JWT.require(Algorithm.HMAC512(SECRET)).build()
+                .verify(tokenValue)
+                .getClaim(MEMBER_ID)
+                .asString();
     }
 }
