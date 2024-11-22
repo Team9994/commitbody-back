53493ec4729dde7b59b45dd225c84c0cb2f6b9d5
CHISANW <keuye06380618@gmail.com>
2024-11-22 18:01:40 +0900

    refactor :CustomExerciseCommentRepositoryImpl 리팩토링
    - 주요 조회 기능 메서드가 지나치게 길고 복잡하여 작은 메서드로 분리
    - 메서드 분리를 통해 코드 가독성과 유지보수성을 향상
    - 공통 로직을 재사용하도록 정리하여 중복 제거
    - 논리적 오류가 있는 로직을 단순화
    - 하드코딩 제거

--- src/main/java/team9499/commitbody/domain/comment/exercise/repository/impl/CustomExerciseCommentRepositoryImpl.java
+++ src/main/java/team9499/commitbody/domain/comment/exercise/repository/impl/CustomExerciseCommentRepositoryImpl.java
@@ -11,15 +11,15 @@ import team9499.commitbody.domain.block.domain.QBlockMember;
 import team9499.commitbody.domain.comment.exercise.domain.ExerciseComment;
 import team9499.commitbody.domain.comment.exercise.dto.ExerciseCommentDto;
 import team9499.commitbody.domain.comment.exercise.repository.CustomExerciseCommentRepository;
+import team9499.commitbody.global.utils.TimeConverter;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static team9499.commitbody.domain.block.domain.QBlockMember.*;
 import static team9499.commitbody.domain.comment.exercise.domain.QExerciseComment.*;
 import static team9499.commitbody.domain.like.domain.QContentLike.*;
+import static team9499.commitbody.global.constants.ElasticFiled.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -27,109 +27,112 @@ public class CustomExerciseCommentRepositoryImpl implements CustomExerciseCommen
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    public static final int SEC = 60;
-    public static final int MIN = 60;
-    public static final int HOUR = 24;
-    public static final int DAY = 30;
-    public static final int MONTH = 12;
-
     private final QBlockMember fromBlock = new QBlockMember("fromBlock");
+
     /**
      * 운동 댓글의 무한 스크롤 메서드
-     * @param memberId  로그인한 사용자 ID
-     * @param exerciseId    운동 ID
-     * @param source    운동 타입 [default : 일반 운동, custom : 사용자 작성 운동]
-     * @param pageable  페이징 정보
-     * @param lastId    마지막 ID 값
-     * @return  Slice 객체를 반환
+     *
+     * @param memberId   로그인한 사용자 ID
+     * @param exerciseId 운동 ID
+     * @param source     운동 타입 [default : 일반 운동, custom : 사용자 작성 운동]
+     * @param pageable   페이징 정보
+     * @param lastId     마지막 ID 값
+     * @return Slice 객체를 반환
      */
     @Override
-    public Slice<ExerciseCommentDto> getExerciseComments(Long memberId, Long exerciseId, String source, Pageable pageable, Long lastId) {
+    public Slice<ExerciseCommentDto> getExerciseComments(Long memberId, Long exerciseId, String source,
+                                                         Pageable pageable, Long lastId) {
+        BooleanBuilder builder = getBuilder(exerciseId, source, lastId);
+        List<ExerciseComment> exerciseCommentsQuery = getExerciseCommentsQuery(memberId, pageable, builder);
+        List<ExerciseCommentDto> commentDtos = getExerciseCommentDtos(memberId, exerciseCommentsQuery);
+        return new SliceImpl<>(commentDtos, pageable, isHasNext(pageable, commentDtos));
+    }
+
+    private BooleanBuilder getBuilder(Long exerciseId, String source, Long lastId) {
         BooleanBuilder builder = new BooleanBuilder();
+        builderLastId(lastId, builder);
+        builderExerciseType(exerciseId, source, builder);
+        return builder;
+    }
 
-        if (lastId!=null) builder.and(exerciseComment.id.lt(lastId));       // 무한 스크롤 다음 페이지가 있을시
-        if (checkExerciseType(source)) builder.and(exerciseComment.exercise.id.eq(exerciseId)); //기본 운동일 경우
-        else builder.and(exerciseComment.customExercise.id.eq(exerciseId));     // 커스텀 운동일 경우
+    private static void builderLastId(Long lastId, BooleanBuilder builder) {
+        if (lastId != null) {
+            builder.and(exerciseComment.id.lt(lastId));
+        }
+    }
 
-        // 쿼리 실행
-        List<ExerciseComment> exerciseComments = jpaQueryFactory.select(exerciseComment)
+    private void builderExerciseType(Long exerciseId, String source, BooleanBuilder builder) {
+        if (checkExerciseType(source)) {
+            builder.and(exerciseComment.exercise.id.eq(exerciseId));
+            return;
+        }
+        builder.and(exerciseComment.customExercise.id.eq(exerciseId));
+    }
+
+    private List<ExerciseComment> getExerciseCommentsQuery(Long memberId, Pageable pageable, BooleanBuilder builder) {
+        return jpaQueryFactory.select(exerciseComment)
                 .from(exerciseComment)
                 .leftJoin(exerciseComment.exerciseCommentLikes, contentLike).fetchJoin()
-                .leftJoin(blockMember).on(blockMember.blocked.id.eq(exerciseComment.member.id).and(blockMember.blocker.id.eq(memberId)))
-                .leftJoin(fromBlock).on(fromBlock.blocked.id.eq(memberId).and(fromBlock.blocker.id.eq(exerciseComment.member.id)))
-                .where(builder,blockMember.id.isNull().or(blockMember.blockStatus.eq(false))
+                .leftJoin(blockMember).on(blockMember.blocked.id.eq(exerciseComment.member.id)
+                        .and(blockMember.blocker.id.eq(memberId)))
+                .leftJoin(fromBlock).on(fromBlock.blocked.id.eq(memberId)
+                        .and(fromBlock.blocker.id.eq(exerciseComment.member.id)))
+                .where(builder, blockMember.id.isNull().or(blockMember.blockStatus.eq(false))
                         .and(fromBlock.id.isNull().or(fromBlock.blockStatus.eq(false)))
                         .and(exerciseComment.member.isWithdrawn.eq(false)))
-                .limit(pageable.getPageSize()+1)
+                .limit(pageable.getPageSize() + 1)
                 .orderBy(exerciseComment.createdAt.desc())      // 최신순을 유지하기 위해 내림차순
                 .fetch();
+    }
 
-
-        List<ExerciseCommentDto> commentDtoList = exerciseComments.stream()
-                .map(ec -> ExerciseCommentDto.of(
-                        ec.getId(),
-                        ec.getMember().getNickname(),
-                        ec.getContent(),
-                        converter(ec.getCreatedAt()),
-                        checkAuthor(memberId, ec.getMember().getId()),
-                        ec.getLikeCount(),
-                        ec.getExerciseCommentLikes() != null && ec.getExerciseCommentLikes().stream().anyMatch(like -> like.getMember().getId().equals(memberId) && like.isLikeStatus())
-                ))
-                .collect(Collectors.toList());
-        // 다음 페이직 존재하는지 검사
+    private static boolean isHasNext(Pageable pageable, List<ExerciseCommentDto> commentDtoList) {
         boolean hasNext = false;
-        if ( commentDtoList.size() > pageable.getPageSize()){
+        if (commentDtoList.size() > pageable.getPageSize()) {
             commentDtoList.remove(pageable.getPageSize());
             hasNext = true;
         }
+        return hasNext;
+    }
 
-       return new SliceImpl<>(commentDtoList,pageable,hasNext);
+    private List<ExerciseCommentDto> getExerciseCommentDtos(Long memberId, List<ExerciseComment> exerciseComments) {
+        return exerciseComments.stream()
+                .map(ec -> createCommentDto(memberId, ec))
+                .collect(Collectors.toList());
+    }
+
+    private ExerciseCommentDto createCommentDto(Long memberId, ExerciseComment ec) {
+        return ExerciseCommentDto.of(
+                ec.getId(),
+                ec.getMember().getNickname(),
+                ec.getContent(),
+                TimeConverter.converter(ec.getCreatedAt()),
+                checkAuthor(memberId, ec.getMember().getId()),
+                ec.getLikeCount(),
+                isLikeStatus(memberId, ec)
+        );
+    }
+
+    private static boolean isLikeStatus(Long memberId, ExerciseComment ec) {
+        return ec.getExerciseCommentLikes() != null && isCheckLike(memberId, ec);
+    }
+
+    private static boolean isCheckLike(Long memberId, ExerciseComment ec) {
+        return ec.getExerciseCommentLikes()
+                .stream()
+                .anyMatch(like -> like.getMember().getId().equals(memberId) && like.isLikeStatus());
     }
     /*
     댓글이 현재 로그인한 사용자인지 확인하는 메서드
     로그인한 사용자가 작성자 일시 : true, 아닐시 : false
      */
     private boolean checkAuthor(Long loginMemberId, Long writer) {
-        return loginMemberId==writer ? true : false;
+        return loginMemberId.equals(writer);
     }
-
-    /*
-    수정된일 기준으로 1일전 ,2주전을 표현학 위한 시간 컨버터
-     */
-    private String converter(LocalDateTime updatedAt) {
-        LocalDateTime now = LocalDateTime.now();
-
-        long diffTime = updatedAt.until(now, ChronoUnit.SECONDS); // now보다 이후면 +, 전이면 -
-
-        if (diffTime < SEC){
-            return diffTime + "초전";
-        }
-        diffTime = diffTime / SEC;
-        if (diffTime < MIN) {
-            return diffTime + "분 전";
-        }
-        diffTime = diffTime / MIN;
-        if (diffTime < HOUR) {
-            return diffTime + "시간 전";
-        }
-        diffTime = diffTime / HOUR;
-        if (diffTime < DAY) {
-            return diffTime + "일 전";
-        }
-        diffTime = diffTime / DAY;
-        if (diffTime < MONTH) {
-            return diffTime + "개월 전";
-        }
-
-        diffTime = diffTime / MONTH;
-        return diffTime + "년 전";
-    }
-
     /*
     운동 제공 타입 검사하는 메서드
     기본 제공 운동 : true , 커스텀 운동 : false
      */
-    private boolean checkExerciseType(String source){
-        return source.equals("default") ? true : false;
+    private boolean checkExerciseType(String source) {
+        return source.equals(DEFAULT);
     }
 }
