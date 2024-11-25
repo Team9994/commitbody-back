b71b22a162a481dff0d7f42d7b9c2bd91f414721
CHISANW <keuye06380618@gmail.com>
2024-11-25 17:04:14 +0900

    refactor : AuthorizationElsService 리팩토링
    - 주요 조회 기능 메서드가 지나치게 길고 복잡하여 작은 메서드로 분리
    - 메서드 분리를 통해 코드 가독성과 유지보수성을 향상
    - 공통 로직을 재사용하도록 정리하여 중복 제거
    - 하드코딩된 값을 상수 클래스를 통해 가독성 향상

--- src/main/java/team9499/commitbody/global/authorization/service/AuthorizationElsService.java
+++ src/main/java/team9499/commitbody/global/authorization/service/AuthorizationElsService.java
@@ -18,6 +18,8 @@ import team9499.commitbody.domain.exercise.service.ElasticExerciseService;
 
 import java.io.IOException;
 
+import static team9499.commitbody.global.constants.ElasticFiled.*;
+
 @Slf4j
 @Service
 @Transactional(transactionManager = "dataTransactionManager")
@@ -28,78 +30,91 @@ public class AuthorizationElsService {
     private final ElsArticleService elsArticleService;
     private final ElasticExerciseService elasticExerciseService;
     private final MemberDocService memberDocService;
-    private final String MEMBER_FIELD = "memberId";
-
 
     /**
      * 비동기를 통한 사용자와 관련된 데이터의 상태를 변경
-     * @param memberId  변경할 사용자 ID
-     * @param status    TURE : 회원 탈퇴 , FALSE : 재가입
+     *
+     * @param memberId 변경할 사용자 ID
+     * @param status   TURE : 회원 탈퇴 , FALSE : 재가입
      */
     @Async
-    public void updateDrawWriteUpdate(Long memberId, String status){
-        boolean type = status.equals("탈퇴");
-        elsArticleService.updateArticleWithDrawAsync(memberId,type);        // 게시글
-        elsArticleService.updateArticleLikeAndCommentCountAsync(memberId,type);     // 게시글 좋아요 댓글수 업데이트
-        elasticExerciseService.updateExerciseInterestWithDrawAsync(memberId,type);  // 관심 운동
-        memberDocService.updateMemberWithDrawAsync(memberId,type); // 사용자
+    public void updateDrawWriteUpdate(Long memberId, String status) {
+        boolean type = status.equals(WRIT_DRAW_KR);
+        elsArticleService.updateArticleWithDrawAsync(memberId, type);        // 게시글
+        elsArticleService.updateArticleLikeAndCommentCountAsync(memberId, type);     // 게시글 좋아요 댓글수 업데이트
+        elasticExerciseService.updateExerciseInterestWithDrawAsync(memberId, type);  // 관심 운동
+        memberDocService.updateMemberWithDrawAsync(memberId, type); // 사용자
     }
 
     @Async
-    public void deleteElsByMemberId(Long memberId){
-        
-        // 운동 필드 삭제
-        DeleteByQueryRequest deleteByExerciseIndex = new DeleteByQueryRequest.Builder().index("exercise_index")
-                .query(getQuery(memberId)).build();
+    public void deleteElsByMemberId(Long memberId) {
+        handlerDeleteMember(memberId,
+                createDeleteExerciseRequest(memberId),
+                createDeleteInterestRequest(memberId),
+                createDeleteMemberRequest(memberId),
+                createDeleteArticleRequest(memberId),
+                createDeleteBlockRequest(memberId),
+                createUpdateQuery(memberId));
+    }
 
-        // 관심 운동 필드 삭제
-        DeleteByQueryRequest deleteInterestIndex = new DeleteByQueryRequest.Builder().index("exercise_interest_index")
-                .query(getQuery(memberId)).build();
-
-        // 사용자 필드 삭제
-        DeleteByQueryRequest deleteByMemberIndex = new DeleteByQueryRequest.Builder().index("member_index")
-                .query(getQuery(memberId)).build();
-
-        // 게시글 필드 삭제
-        DeleteByQueryRequest deleteByArticleRequest = new DeleteByQueryRequest.Builder().index("article_index")
-                .query(getQuery(memberId)).build();
-
-        // 차단된 사용자 필드 삭제
-        DeleteByQueryRequest deleteByBlockRequest = new DeleteByQueryRequest.Builder().index("block_member_index")
-                .query(Query.of(q -> q.term(t -> t.field("id").value("blocked_"+memberId))))
-                .build();
-
-        // 차단한 사용자 목록에서 사용자 제거
-        UpdateByQueryRequest request = new UpdateByQueryRequest.Builder()
-                .index("block_member_index")  // 인덱스 이름 설정
-                .query(q -> q
-                        .term(t -> t
-                                .field("blockerId")  // 업데이트할 조건 필드
-                                .value(memberId)           // 해당 필드의 값
-                        )
-                )
-                .script(s -> s.inline(i -> i.source("ctx._source.blockerId.removeIf(id -> id == params.id)")
-                        .params("id", JsonData.of(memberId)))
-                )
-                .build();
-
+    private void handlerDeleteMember(Long memberId, DeleteByQueryRequest deleteByExerciseIndex, DeleteByQueryRequest deleteInterestIndex, DeleteByQueryRequest deleteByMemberIndex, DeleteByQueryRequest deleteByArticleRequest, DeleteByQueryRequest deleteByBlockRequest, UpdateByQueryRequest request) {
         try {
             DeleteByQueryResponse deleteByExerciseResponse = elasticsearchClient.deleteByQuery(deleteByExerciseIndex);
-            DeleteByQueryResponse deleteByInterestResponse= elasticsearchClient.deleteByQuery(deleteInterestIndex);
-            DeleteByQueryResponse deleteByMemberResponse= elasticsearchClient.deleteByQuery(deleteByMemberIndex);
-            DeleteByQueryResponse deleteByArticleResponse= elasticsearchClient.deleteByQuery(deleteByArticleRequest);
-            DeleteByQueryResponse deleteByBlockResponse= elasticsearchClient.deleteByQuery(deleteByBlockRequest);
+            DeleteByQueryResponse deleteByInterestResponse = elasticsearchClient.deleteByQuery(deleteInterestIndex);
+            DeleteByQueryResponse deleteByMemberResponse = elasticsearchClient.deleteByQuery(deleteByMemberIndex);
+            DeleteByQueryResponse deleteByArticleResponse = elasticsearchClient.deleteByQuery(deleteByArticleRequest);
+            DeleteByQueryResponse deleteByBlockResponse = elasticsearchClient.deleteByQuery(deleteByBlockRequest);
             UpdateByQueryResponse updateByQueryResponse = elasticsearchClient.updateByQuery(request);
             log.info("{}번 회원 탈퇴시 엘라스틱 데이터 삭제 [운동 : {}, 관심 운동 : {}, 게시글 : {}, 사용자 : {} , 차단 목록 업데이트 : {}, 차단 사용자 삭제 : {}]",
-                    memberId,deleteByExerciseResponse.deleted(),deleteByInterestResponse.deleted(),deleteByArticleResponse.deleted(),
-                    deleteByMemberResponse.deleted(),updateByQueryResponse.updated(),deleteByBlockResponse.deleted());
-
+                    memberId, deleteByExerciseResponse.deleted(), deleteByInterestResponse.deleted(), deleteByArticleResponse.deleted(),
+                    deleteByMemberResponse.deleted(), updateByQueryResponse.updated(), deleteByBlockResponse.deleted());
         } catch (IOException e) {
-            log.error("회원 탈퇴시 엘라스틱 데이터 삭제 도중 오류 발생 : {}",e.getMessage());
+            log.error("회원 탈퇴시 엘라스틱 데이터 삭제 도중 오류 발생 : {}", e.getMessage());
         }
     }
 
+    private static UpdateByQueryRequest createUpdateQuery(Long memberId) {
+        return new UpdateByQueryRequest.Builder()
+                .index(BLOCK_MEMBER_INDEX)  // 인덱스 이름 설정
+                .query(q -> q
+                        .term(t -> t
+                                .field(BLOCKER_ID)  // 업데이트할 조건 필드
+                                .value(memberId)           // 해당 필드의 값
+                        )
+                )
+                .script(s -> s.inline(i -> i.source(BLOCKER_REMOVE_IF)
+                        .params(ID, JsonData.of(memberId)))
+                )
+                .build();
+    }
+
+    private static DeleteByQueryRequest createDeleteBlockRequest(Long memberId) {
+        return new DeleteByQueryRequest.Builder().index(BLOCK_MEMBER_INDEX)
+                .query(Query.of(q -> q.term(t -> t.field(ID).value(BLOCKED_ + memberId))))
+                .build();
+    }
+
+    private DeleteByQueryRequest createDeleteArticleRequest(Long memberId) {
+        return new DeleteByQueryRequest.Builder().index(ARTICLE_INDEX)
+                .query(getQuery(memberId)).build();
+    }
+
+    private DeleteByQueryRequest createDeleteMemberRequest(Long memberId) {
+        return new DeleteByQueryRequest.Builder().index(MEMBER_INDEX)
+                .query(getQuery(memberId)).build();
+    }
+
+    private DeleteByQueryRequest createDeleteInterestRequest(Long memberId) {
+        return new DeleteByQueryRequest.Builder().index(INTEREST_INDEX)
+                .query(getQuery(memberId)).build();
+    }
+
+    private DeleteByQueryRequest createDeleteExerciseRequest(Long memberId) {
+        return new DeleteByQueryRequest.Builder().index(EXERCISE_INDEX)
+                .query(getQuery(memberId)).build();
+    }
+
     private Query getQuery(Long memberId) {
-        return Query.of(q -> q.term(t -> t.field(MEMBER_FIELD).value(memberId)));
+        return Query.of(q -> q.term(t -> t.field(MEMBER_ID).value(memberId)));
     }
 }
