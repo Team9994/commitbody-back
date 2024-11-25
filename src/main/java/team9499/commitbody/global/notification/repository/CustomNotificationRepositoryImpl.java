0d4fcdb801f2bb8e3e96e1a446c317dc969cba72
CHISANW <keuye06380618@gmail.com>
2024-11-25 17:05:07 +0900

    refactor : CustomNotificationRepositoryImpl 리팩토링
    - 주요 조회 기능 메서드가 지나치게 길고 복잡하여 작은 메서드로 분리
    - 메서드 분리를 통해 코드 가독성과 유지보수성을 향상
    - 공통 로직을 재사용하도록 정리하여 중복 제거
    - 하드코딩된 값을 상수 클래스를 통해 가독성 향상

--- src/main/java/team9499/commitbody/global/notification/repository/CustomNotificationRepositoryImpl.java
+++ src/main/java/team9499/commitbody/global/notification/repository/CustomNotificationRepositoryImpl.java
@@ -4,49 +4,60 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 import team9499.commitbody.global.notification.dto.NotificationDto;
-import team9499.commitbody.global.notification.domain.Notification;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static team9499.commitbody.global.notification.domain.QNotification.*;
 
-@Slf4j
 @Repository
 @RequiredArgsConstructor
-public class CustomNotificationRepositoryImpl implements CustomNotificationRepository{
+public class CustomNotificationRepositoryImpl implements CustomNotificationRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-
     @Override
     public Slice<NotificationDto> getAllNotification(Long memberId, Long lastId, Pageable pageable) {
+        BooleanBuilder lastIdBuilder = lastIdBuilder(lastId);
+        List<Tuple> notificationQuery = notificationQuery(memberId, pageable, lastIdBuilder);
+        List<NotificationDto> notificationDtos = getNotificationDtos(notificationQuery);
+        return new SliceImpl<>(notificationDtos, pageable, isHasNext(pageable, notificationDtos));
+    }
+
+    private static BooleanBuilder lastIdBuilder(Long lastId) {
         BooleanBuilder lastIdBuilder = new BooleanBuilder();
         if (lastId != null) {
             lastIdBuilder.and(notification.id.lt(lastId));
         }
-        List<Tuple> tupleList = jpaQueryFactory.select(notification, notification.receiver, notification.sender)
+        return lastIdBuilder;
+    }
+
+    private List<Tuple> notificationQuery(Long memberId, Pageable pageable, BooleanBuilder lastIdBuilder) {
+        return jpaQueryFactory.select(notification, notification.receiver, notification.sender)
                 .from(notification)
                 .where(lastIdBuilder, notification.receiver.id.eq(memberId))
                 .limit(pageable.getPageSize() + 1)
                 .orderBy(notification.createdAt.desc())
                 .fetch();
+    }
 
-        List<NotificationDto> notificationDtoList = tupleList.stream().map(t -> NotificationDto.of(t.get(notification))).collect(Collectors.toList());
+    private static List<NotificationDto> getNotificationDtos(List<Tuple> notificationQuery) {
+        return notificationQuery.stream().map(t -> NotificationDto.of(t.get(notification))).collect(Collectors.toList());
+    }
 
+    private static boolean isHasNext(Pageable pageable, List<NotificationDto> notificationDtos) {
         boolean hasNext = false;
 
-        if (notificationDtoList.size() > pageable.getPageSize()){
-            notificationDtoList.remove(pageable.getPageSize());
+        if (notificationDtos.size() > pageable.getPageSize()) {
+            notificationDtos.remove(pageable.getPageSize());
             hasNext = true;
         }
-
-        return new SliceImpl<>(notificationDtoList,pageable,hasNext);
+        return hasNext;
     }
+
 }
