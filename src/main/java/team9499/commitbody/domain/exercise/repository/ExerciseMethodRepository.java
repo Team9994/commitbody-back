3a926bf46e01d353afacd5fa6cd8a9965757cc63
CHISANW <keuye06380618@gmail.com>
2024-12-23 14:45:50 +0900

    feat : 운동 방법 조회 jpa 추가

--- src/main/java/team9499/commitbody/domain/exercise/repository/ExerciseMethodRepository.java
+++ src/main/java/team9499/commitbody/domain/exercise/repository/ExerciseMethodRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team9499.commitbody.domain.exercise.domain.ExerciseMethod;
 
+import java.util.List;
+
 @Repository
 public interface ExerciseMethodRepository extends JpaRepository<ExerciseMethod, Long> {
+
+    List<ExerciseMethod> findAllByExerciseId(Long exerciseId);
+    
 }
